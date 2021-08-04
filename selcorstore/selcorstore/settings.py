f4cc039674aefe25d02a86ce0dcc86b668578501
Linux Kyrios <malinowskim92@protonmail.com>
2021-08-04 21:59:26 +0200

    Installed Pillow, modified settings.py for media usage, modified urls.py for media usage

--- selcorstore/selcorstore/settings.py
+++ selcorstore/selcorstore/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -132,3 +132,7 @@ LOGOUT_URL = 'logout'
 
 #Temporary solution to change in final stage
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# For adding pillow usage
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
