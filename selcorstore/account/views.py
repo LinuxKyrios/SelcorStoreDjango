82767a59524b5fa4cbc248da8357e7953183da48
Linux Kyrios <malinowskim92@protonmail.com>
2021-08-07 20:42:00 +0200

    Added view for profile edition

--- selcorstore/account/views.py
+++ selcorstore/account/views.py
@@ -61,3 +61,21 @@ def register(request):
 
 
 # View for profile edition
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user,
+                                 data=request.POST)
+        profile_form = ProfileEditForm(instance=request.user.profile,
+                                       data=request.POST,
+                                       files=request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+    return render(request,
+                  'account/edit.html',
+                  {'user_form': user_form,
+                   'profile_form': profile_form})
