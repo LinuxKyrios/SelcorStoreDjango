f9d4b5e2e51e5d436ce5b7fc435f0ede6510f2c7
Linux Kyrios <malinowskim92@protonmail.com>
2021-08-08 23:34:45 +0200

    modified urls.py file of selcorshop application to add routing, and modified urls.py file of selcorstore project. Typo in views.py improved

--- selcorstore/selcorstore/urls.py
+++ selcorstore/selcorstore/urls.py
@@ -20,7 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('account/', include('account.urls'))
+    path('account/', include('account.urls')),
+    path('', include('selcorshop.urls', namespace='selcorshop'))
 ]
 
 if settings.DEBUG:
