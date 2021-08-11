283846f86c05e40b8d8f24ecafaba7c812c724ee
Linux Kyrios <malinowskim92@protonmail.com>
2021-08-11 21:39:49 +0200

    Added methods for removing product from the cart, iteration through elements of the art, uploading from database, counting all elements in the cart, and removing cart from session

--- selcorstore/cart/cart.py
+++ selcorstore/cart/cart.py
@@ -31,3 +31,43 @@ class Cart(object):
 
     def save(self):
         self.session.modified = True
+
+    def remove(self, product):
+        """
+        Removing product from the cart
+        """
+        product_id = str(product.id)
+        if product_id in self.cart:
+            del self.cart[product_id]
+            self.save()
+
+    def __iter__(self):
+        """
+        Iteration through elements of the cart and uploading products from database
+        """
+        product_ids = self.cart.keys()
+        # uploading product objects and adding them to the cart
+        products = Product.objects.filter(id__in=product_ids)
+
+        cart = self.cart.copy()
+        for product in products:
+            cart[str(product.id)]['product'] = product
+            for item in cart.values():
+                item['price'] = Decimal(item['prices'])
+                item['total_price'] = item['price'] * item['quantity']
+                yield item
+
+    def __len__(self):
+        """
+        Counting all elements in the cart
+        """
+        return sum(item['quantity'] for item in self.cart.values())
+
+    # Method for counting all elements in the cart
+    def get_total_price(self):
+        return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values())
+
+    def clear(self):
+        # removing cart from session
+        del self.session[settings.CART_SESSION_ID]
+        self.save()
