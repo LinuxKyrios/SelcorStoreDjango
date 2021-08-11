8248de0b5b103ac4dc52d653b530e0b1c51786a6
Linux Kyrios <malinowskim92@protonmail.com>
2021-08-12 01:30:00 +0200

    Typos improved

--- selcorstore/orders/views.py
+++ selcorstore/orders/views.py
@@ -12,11 +12,17 @@ def order_create(request):
         if form.is_valid():
             order = form.save()
             for item in cart:
-                OrderItem.objects.create(order=order, product=item['product'], price=item['price'],
+                OrderItem.objects.create(order=order,
+                                         product=item['product'],
+                                         price=item['price'],
                                          quantity=item['quantity'])
                 # Removing content of the cart
                 cart.clear()
-                return render(request, 'orders/order/created.html', {'order': order})
+                return render(request,
+                              'orders/order/created.html',
+                              {'order': order})
     else:
         form = OrderCreateForm()
-    return render(request, 'orders/order/created.html', {'cart': cart, 'form': form})
+    return render(request,
+                  'orders/order/create.html',
+                  {'cart': cart, 'form': form})
